e423d4826822df372a948f7416387659fa5a2118
Jean Matbagan <matbaganjean@gmail.com>
2024-10-15 21:06:42 +0800

    Add files via upload

---
+++ arrayActivity.js
@@ -0,0 +1,12 @@
+//Array that called favsingers
+let favSingers = ["Rose","Chen","Sarah"];
+console.log(favSingers[0]); //log the 1st singer using array index notation
+//Array that called favsingers and mixed value data type
+let favNumbers = ["28","25","63","16"];
+let mixedArr = ["Kimi",["F1"],123,true];
+
+//Accessing and printing each item in the mixedArr
+console.log(mixedArr[0]); 
+console.log(mixedArr[1][0]); 
+console.log(mixedArr[2]); 
+console.log(mixedArr[3]); 
